4bec3ca26630f95ca4b46b75d1b5cecdf9b69b59
Harshda Mhase <harshadamhase12@gmail.com>
2023-04-21 10:00:35 -0900

    jsx added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(
+     <div>
+          <h1>first fight</h1>
+          <h2>second flight</h2>
+          <img src="https://th.bing.com/th/id/OIP.HxV79tFMPfBAIo0BBF-sOgHaEy?pid=ImgDet&rs=1" alt='img'/>
+     
+     </div>);
 
 
