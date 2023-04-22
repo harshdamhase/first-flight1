ca23961d9a32e6f9e126e80279cff42117139e21
Harshda Mhase <harshadamhase12@gmail.com>
2023-04-21 21:54:20 -0900

    first react mini proj

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+   return (
+       <div>
+        <App/>
+        <h2>App</h2>
+        </div>
+   )  
+
+     }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 
+const studname="harshda";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-     <div>
-          <h1>first fight</h1>
-          <h2>second flight</h2>
-          <img src="https://th.bing.com/th/id/OIP.HxV79tFMPfBAIo0BBF-sOgHaEy?pid=ImgDet&rs=1" alt='img'/>
-     
-     </div>);
+root.render(<div>
+    <h1>First Flight</h1>
+    <h1>{studname}</h1>
+    <h3>{5+5}</h3>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphoto%2Fcheering-woman-open-arms-to-sunrise-at-seaside-gm523185547-51198600&psig=AOvVaw1E8Zvf_YsxPGkhFBeTBvza&ust=1682179801099000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPjWl9DwvP4CFQAAAAAdAAAAABAJ" alt="img"/>
+</div>);
 
 
