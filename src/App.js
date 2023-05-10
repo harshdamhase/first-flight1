dd02bab835074e88e4183d6ec7f67d80a3dfd292
Harshda Mhase <harshadamhase12@gmail.com>
2023-05-10 12:03:31 -0900

    bootstrap install in this proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 
 function App() {
    return (
        <div>
        
-        <h2>App</h2>
+        <h2 className="card">App</h2>
    
         </div>
    )  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 ;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<>
